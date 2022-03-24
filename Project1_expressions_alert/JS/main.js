f90918d052f8783622114a3c7b88a7109794e536
Abdal Chowdhury <88795186+Abdalchy@users.noreply.github.com>
2022-03-24 16:31:19 -0400

    Add files via upload
    File addition for Assignment 1

---
+++ Project1_expressions_alert/JS/main.js
@@ -0,0 +1,17 @@
+/*alert("Main Hello, World!");*/
+
+/*window.alert("Window Alert Hello, World!");*/
+
+var A = "This is a String";
+var B = "Assignment 7 String";
+var C = "\"It's this way\" \\ \"Or maybe this way\"";
+var D = "\"There will come a day," + " maybe not today," + " but it will come.\"";
+var E = "For Challenge 1," + " we were asked to write a variable." + " The variable should contain a concatenated string.";
+
+var assignment12 = (3+3);
+
+function My_First_Function() {	//Define and name function
+	var str = "This text is green!";	//Define variable and give string value
+	var result = str.fontcolor("green");	//Font color
+	document.getElementById("Green_Text") .innerHTML = result;	//Value of result
+}
