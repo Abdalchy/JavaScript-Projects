e56836382425ae429e74cbe6b34effabdeaf7912
Abdal Chowdhury <88795186+Abdalchy@users.noreply.github.com>
2022-03-30 13:20:40 -0400

    Project 2 functions added

---
+++ Project2_functions/js/main.js
@@ -0,0 +1,6 @@
+function function1() {
+	var sentence = "JavaScript has been hard,";
+	sentence += " but I think I am understanding!";
+	document.getElementById("Concatenate") .innerHTML = sentence;
+}
+
